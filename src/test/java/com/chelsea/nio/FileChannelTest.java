abd80694c4f565a38fdfdece79d8f34651e2eb16
412745310@qq.com <412745310@qq.com>
2018-01-31 14:39:47 +0800

    新增FileChannel测试类

---
+++ src/test/java/com/chelsea/nio/FileChannelTest.java
@@ -0,0 +1,39 @@
+package com.chelsea.nio;
+
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+
+import org.junit.Test;
+
+/**
+ * FileChannel测试类
+ * 
+ * @author shevchenko
+ *
+ */
+public class FileChannelTest {
+
+    @SuppressWarnings("resource")
+    @Test
+    public void testTransferFrom() throws Exception {
+        RandomAccessFile fromFile = new RandomAccessFile("C:/Users/Administrator/Desktop/fromFile.txt", "rw");
+        FileChannel fromChannel = fromFile.getChannel();
+        RandomAccessFile toFile = new RandomAccessFile("C:/Users/Administrator/Desktop/toFile.txt", "rw");
+        FileChannel toChannel = toFile.getChannel();
+        long position = 1;
+        long count = fromChannel.size();
+        toChannel.transferFrom(fromChannel, position, count);
+    }
+
+    @SuppressWarnings("resource")
+    @Test
+    public void testTransferTo() throws Exception {
+        RandomAccessFile fromFile = new RandomAccessFile("C:/Users/Administrator/Desktop/fromFile.txt", "rw");
+        FileChannel fromChannel = fromFile.getChannel();
+        RandomAccessFile toFile = new RandomAccessFile("C:/Users/Administrator/Desktop/toFile.txt", "rw");
+        FileChannel toChannel = toFile.getChannel();
+        long position = 1;
+        long count = fromChannel.size();
+        fromChannel.transferTo(position, count, toChannel);
+    }
+}
